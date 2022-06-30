53548ca982f197945962b34b67d0651144cc8529
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2022-06-30 19:11:23 -0300

    button game2d effect (css)

--- src/contact/Form.jsx
+++ src/contact/Form.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Form = () => {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
+  console.log(data);
+  
   return (
     <div>
-      <form>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Text:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
+      <h1>Contact</h1>
+      <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+        <input {...register("Name")} placeholder="Name" />
+        <textarea {...register("Message")} placeholder="Message" />
+        <input type="submit" />
       </form>
     </div>
-  )
+  );
 }
 
 export default Form
--- src/content/Work.jsx
+++ src/content/Work.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import styles from '../content/Work.module.css'
+import data from '../workData.json'
+
 
 function Work() {
   return (
     <section className={styles.sectionWork}>
         <h1>Work</h1>
         <div className={styles.workContainer}>
-            <div className={styles.workCard}>Blog</div>
-            <div className={styles.workCard}>Godot Game</div>
-            <div className={styles.workCard}>Meme Generator</div>
-            <div className={styles.workCard}>Movie Search</div>
+          {data.map((work => {
+            return (
+              <div className={styles.workCard}>
+                {/* atributo target (HTML) para abrir link en una ventana nueva y rel para seguridad*/ }
+                <a href={work.link} target="_blank" rel="noopener noreferrer">
+                  <img src={work.img} />
+                </a>
+                <div className={styles.workTittle}>
+                  <h3>{work.name}</h3>
+                  <p>{work.description}</p>
+                </div>
+              </div>
+            )
+          }))}
         </div>
     </section>
   )
